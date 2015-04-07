44d9f98c7b2041e7041a51f6b32385b0904a2d17
Srimal Priyanga <s.priyanga22@gmail.com>
2015-04-07 22:49:35 +0530

    operations created and boolien values updates

--- Truth-Tables_Prolog-Project.pl
+++ Truth-Tables_Prolog-Project.pl
@@ -1,7 +1,12 @@
 % Team D2Real
 
+% Create operators.
+:- operation(1000,inf,'and').
+:- operation(1000,inf,'or').
+:- operation(900,una,'not').
+
 % Create Boolian Values for and, or, & not
-bool_not (0,1).
+bool_not(0,1).
 bool_not (1,0).
 bool_or (0,1).
 bool_or (1,0).
