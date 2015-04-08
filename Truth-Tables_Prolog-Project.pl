57d02e942e7c68f59659a923dfc81a84f790299a
Srimal Priyanga <s.priyanga22@gmail.com>
2015-04-08 23:57:54 +0530

    Extract the variables from the boolean expression

--- Truth-Tables_Prolog-Project.pl
+++ Truth-Tables_Prolog-Project.pl
@@ -5,6 +5,17 @@
 :- operation(1000,inf,'or').
 :- operation(900,una,'not').
 
+% Extract the variables from the boolean expression.
+find_vars(N,V,V) :- member(N,[0,1]),!.    /* Boolean constants in expression */
+find_vars(X,Vin,Vout) :- atom(X),
+                         (member(X,Vin) -> Vout = Vin ;   /* already have  */
+                            Vout = [X|Vin]).              /* include       */
+find_vars(X and Y,Vin,Vout) :- find_vars(X,Vin,Vtemp),
+                               find_vars(Y,Vtemp,Vout).
+find_vars(X or Y,Vin,Vout) :-  find_vars(X,Vin,Vtemp),
+                               find_vars(Y,Vtemp,Vout).
+find_vars(not X,Vin,Vout) :-   find_vars(X,Vin,Vout).
+
 % Create Boolian Values for and, or, & not
 bool_not(0,1).
 bool_not(1,0).
