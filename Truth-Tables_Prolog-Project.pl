52000713db942386919329378345ce2f741c7cff
Srimal Priyanga <s.priyanga22@gmail.com>
2015-04-10 07:56:42 +0530

    usage comments and next truth value genarate

--- Truth-Tables_Prolog-Project.pl
+++ Truth-Tables_Prolog-Project.pl
@@ -1,10 +1,9 @@
-% Team D2Real NSBM-CS Batch
+% Team D2Real NSBM-CS Batch.04
 
 % Create operators. 'inf'=infix, 'una'=unary
 :- op(1000,inf,'and').
 :- op(1000,inf,'or').
 :- op(900,una,'not').
-
 
 % Extract the variables from the boolean expression.
 find_vars(N,V,V) :- member(N,[0,1]),!.
@@ -17,49 +16,50 @@ find_vars(X or Y,Vin,Vout) :-  find_vars(X,Vin,Vtemp),
                                find_vars(Y,Vtemp,Vout).
 find_vars(not X,Vin,Vout) :-   find_vars(X,Vin,Vout).
 
-%Find the initial assignments
+%Find the initial assignments of variables
 initial_assignment1([],[]).
 initial_assignmet1([_X|R],[0|S]) :- initial_assignment1(R,S).
 
-
-% Changes the truth assignments by doing a binary addition with 1
+% truth assignments changes by binary addition with 1
 next([0|R],[1|R]).
 next([1|R],[0|S]) :- next(R,S).
 
-
 % Reverse a list
 reverse([],[]).
-reverse([P],[P]).
-reverse(M,N) :- reverse(M,[],N).
+reverse([H],[H]).
+reverse(A,B) :- reverse(A,[],B).
 reverse([],R,R).
-reverse([P|T],S,L) :- reverse(T,[P|S],L).
+reverse([H|T],S,L) :- reverse(T,[H|S],L).
 
+% The next set of truth table values
+successor(A,S) :- reverse(A,R),
+                  next(R,N),
+                  reverse(N,S).
 
+% Create Boolean Values for and, or, & not
+boolean_and(0,0,0).
+boolean_and(0,1,0).
+boolean_and(1,0,0).
+boolean_and(1,1,1).
+boolean_or(0,0,0).
+boolean_or(0,1,1).
+boolean_or(1,0,1).
+boolean_or(1,1,1).
+boolean_not(0,1).
+boolean_not(1,0).
 
-% Create Boolian Values for and, or, & not
-boole_and(0,0,0).
-boole_and(0,1,0).
-boole_and(1,0,0).
-boole_and(1,1,1).
-boole_or(0,0,0).
-boole_or(0,1,1).
-boole_or(1,0,1).
-boole_or(1,1,1).
-boole_not(0,1).
-boole_not(1,0).
-
-% Returns the truth value for the passed expression
+% Truth value returns for Passed expresion
 truth_value(N,_,_,N) :- member(N,[0,1]).
 truth_value(X,Vars,A,Val) :- atom(X),
                              lookup(X,Vars,A,Val).
 truth_value(X and Y,Vars,A,Val) :- truth_value(X,Vars,A,VX),
                                    truth_value(Y,Vars,A,VY),
-                                   boole_and(VX,VY,Val).
+                                   boolean_and(VX,VY,Val).
 truth_value(X or Y,Vars,A,Val) :-  truth_value(X,Vars,A,VX),
                                    truth_value(Y,Vars,A,VY),
-                                   boole_or(VX,VY,Val).
+                                   boolean_or(VX,VY,Val).
 truth_value(not X,Vars,A,Val) :-   truth_value(X,Vars,A,VX),
-                                   boole_not(VX,Val).
+                                   boolean_not(VX,Val).
 
 
 
@@ -83,7 +83,8 @@ write_a_row(E,Vars,A) :- write('  '), write(A), write('        '),
 
 
 
-
+% How to use:
+% tt(x or (not y  and z)).
 
 
 
